0aa5411af3c4b117ff36020e97044e09b4576234
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-02-02 15:28:15 +0700

    updated

--- server/server.py
+++ server/server.py
@@ -1,32 +1,40 @@
 from flask import Flask, request, abort, jsonify, render_template
+import jwt
 import json
+import hashlib
 import datetime
 
 from db_api import *
 
 app = Flask(__name__, static_folder="../dist", template_folder="../static")
 
+SECRET_KEY = 'my_secret_secret_key'
+HASH_KEY = 'my_secret_hash_key'
+
+def encode(string):
+    return hashlib.sha1(str.encode(string)).hexdigest()
 
 @app.route('/')
 def index():
  return render_template("index.html")
 
-
 @app.route('/compile', methods=['POST'])
 def post_program():
-    filename = request.json['file_name']
-    compiler_id = request.json['lang']
-    user_id = request.json['uid']
-    # task_id = request.json['task_id']
+    filename = request.json['filename']
+    compiler_id = request.json['compiler_id']
+    username = request.json['username']
+    with DatabaseConnection() as dbconn:
+        user_id = dbconn.get_uid_by_username(username)
+    task_id = request.json['task_id']
     source = request.json['source']
-    if filename is None or compiler_id is None or user_id is None or source is None:
-        app.logger.error("Not enough info about compiling program")
-        abort(400, message="Enter missing info")
+    if filename is None or compiler_id is None or user_id is None or task_id is None or source is None:
+        app.logger.error('Not enough info about compiling program')
+        abort(400, message='Enter missing info')
     data = {
         'filename' : filename,
         'compiler_id' : compiler_id,
         'user_id' : user_id,
-        'task_id' : '1',
+        'task_id' : task_id,
         'source' : source
     }
     print(json.dumps(data))
@@ -34,9 +42,46 @@ def post_program():
         dbconn.insert_raw_commit(json.dumps(data))
     return jsonify({"result" : "success"}), 200
 
-@app.route('/lang', methods=['GET'])
-def get_lang():
-        return ''
+@app.route('/auth/register', methods=['POST'])
+def post_user():
+    login = request.json['login']
+    password = request.json['password']
+    with DatabaseConnection() as dbconn:
+        dbconn.register_user(login, encode(password))
+    return jsonify({ 'status': 'success' })
+
+@app.route('/auth/login', methods=['POST'])
+def get_user():
+    login = request.json['login']
+    password = request.json['password']
+    hashed_password = encode(password)
+    with DatabaseConnection() as dbconn:
+        hash_from_db = dbconn.get_hash_by_username(login)
+    if hashed_password == hash_from_db:
+        token = jwt.encode({ 'password' : password }, SECRET_KEY, algorithm='HS256')
+        return jsonify({ 'token' : token.decode('ascii') })
+      
+@app.route('/auth/verify', methods=['POST'])
+def verify_user():
+    print(request)
+    print(request.json)
+    username = request.json['username']
+    token = request.headers.get('Authorization')
+    payload = jwt.decode(token.encode('ascii'), SECRET_KEY)
+    password_hash = encode(payload['password'])
+    with DatabaseConnection() as dbconn:
+        hash_from_db = dbconn.get_hash_by_username(username)
+    if password_hash == hash_from_db:
+        return jsonify({ 'status': 'success' }), 200
+    return jsonify({ 'status': 'danger' }), 400
+
+@app.route('/compilers', methods=['GET'])
+def get_compilers():
+    return ''
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return ''
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5002, debug=True)
